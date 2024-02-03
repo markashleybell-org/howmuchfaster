2a9cd593a36078e90aa9a29da71f8676b3c9692a
Mark Bell <me@markb.co.uk>
2024-02-03 14:07:35 +0000

    Basic initial app

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-/* <Query Kind="Program" />
-
-void Main()
-{
-	// Increase
-
-	// var before = 49.6M;
-	var before = 363.8M;
-	var after = 225.3M;
-
-	(((after - before) / before) * 100).ToString("0.0\\%").Dump("Performance increase");
-	(after / before).ToString("0x").Dump("Times faster");
-
-	// Reduction
-    
-    // https://stackoverflow.com/questions/28403939/how-to-calculate-percentage-improvement-in-response-time-for-performance-testing
-    
-    // var then =  1000M;
-	var then =  363.8M;
-    var now = 225.3M;
-
-    // (((then - now) / then) * 100).ToString("0.0\\%").Dump("Execution time reduction");
-    (((then - now) / now) * 100).ToString("0.0\\%").Dump("Performance increase");
-    (then / now).ToString("0x").Dump("Times faster");
-
-
-	// Size decrease
-
-	// var then =  1000M;
-	var pre = 363.8M;
-	var post = 225.3M;
-
-	(((pre - post) / pre) * 100).ToString("0.0\\%").Dump("Size reduction");
-	(pre / post).ToString("0x").Dump("Times faster");
-} */
-
-var HMF = (function() {
-    function calculatePerformanceIncrease() {
-        console.log('DO IT')
+var HMF = (function () {
+    function calculatePercentageDifference(original, final) {
+        return ((final - original) / original) * 100;
     }
+
+    Array.from(document.querySelectorAll('[data-expand]'), input => {
+        let parent = input.parentNode;
+        function updateSize() {
+            parent.dataset.value = input.value;
+        }
+        input.addEventListener('input', updateSize);
+        updateSize();
+    });
+
+    const form = document.querySelector('form');
+    const originalInput = document.querySelector('[name=original]');
+    const finalInput = document.querySelector('[name=final]');
+    const resultPlaceholder = document.querySelector('.result-placeholder');
+    const result = document.querySelector('.result');
+
+    function validate() {
+        if (!form.checkValidity()) {
+            result.classList.add('hidden');
+            resultPlaceholder.classList.remove('hidden');
+            return;
+        }
+
+        const original = parseFloat(originalInput.value);
+        const final = parseFloat(finalInput.value);
+
+        const percentage = calculatePercentageDifference(original, final);
+        const speedup = original > final ? original / final : final / original;
+
+        result.innerText = `That's a ${percentage.toFixed(2)}% ${percentage < 0 ? 'reduction' : 'increase'}, or ${speedup.toFixed(1)}x the baseline.`;
+
+        result.classList.remove('hidden');
+        resultPlaceholder.classList.add('hidden');
+    }
+
+    originalInput.addEventListener('input', validate);
+    finalInput.addEventListener('input', validate);
+
     return {
-        calculatePerformanceIncrease
-    }
+        calculatePercentageDifference
+    };
 })();
